61178d75b76016ed8f84083987a47f88c0240a66
pinyin <bob432091@gmail.com>
2018-12-16 20:03:30 +0800

    improve performance

--- src/useRxjs.ts
+++ src/useRxjs.ts
@@ -1,19 +1,19 @@
 import { OperatorFunction, Subject, Subscription } from 'rxjs'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export function useRxjs<A, S>(
     op: OperatorFunction<A, S>,
     initialValue: S,
 ): [S, (a: A) => void] {
     const [state, setState] = useState(initialValue)
-    const subject = useRef(new Subject<A>())
+    const subject = useMemo(() => new Subject<A>(), [])
 
     useEffect(() => {
         const subscription = new Subscription()
-        subscription.add(subject.current)
-        subscription.add(subject.current.pipe(op).subscribe(s => setState(s)))
+        subscription.add(subject)
+        subscription.add(subject.pipe(op).subscribe(s => setState(s)))
         return () => subscription.unsubscribe()
     }, [])
 
-    return [state, a => subject.current.next(a)]
+    return [state, a => subject.next(a)]
 }
